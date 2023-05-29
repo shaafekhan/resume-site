ef8d0749f8c4368114585fbb02470b29554be702
shaafekhan <56558384+shaafekhan@users.noreply.github.com>
2023-05-30 01:46:14 +0200

    add js code

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+
+let obo = {
+    getcount() {
+        let test = 4
+        const countVisit = document.getElementById("count");
+        countVisit.textContent = test;
+    },
+}
+
+obo.getcount();
